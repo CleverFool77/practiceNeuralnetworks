e53cc7831ad0aa88e911fdbabd0f8fda655d0282
Lekhika Dugtal <lekhidugtal@gmail.com>
2018-12-27 22:43:55 +0530

    add first example

---
+++ example1.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+class NeuralNetwork():
+    def __init__(self):
+        np.random.seed(1)
+        self.synaptic_weights = 2*np.random.random((3,1)) - 1
+    
+    def sigmoid_func(self,x):
+        a = 1/(1+np.exp(-x))
+        return a
+
+    def derivative_sigmoid(self,x):
+        a = x*(1-x)
+        return a
+    
+    def predict(self,input):
+        input = input.astype(float)
+        p = np.dot(input,self.synaptic_weights)
+        r_output = self.sigmoid(p)
+        print(r_output)
+
+    
+    def train(self,input,output,interation):
+        for i in range(interation):
+            r_input = input.astype(float)
+            p = np.dot(r_input,self.synaptic_weights)
+            r_output = self.sigmoid_func(p)
+
+            e = output - r_output
+            delta = e*self.derivative_sigmoid(output)
+            weight_change = np.dot(input.T , e * self.derivative_sigmoid(r_output))
+            self.synaptic_weights =  self.synaptic_weights + weight_change
+
+
+if __name__ == "__main__":
+    neuralnetwork = NeuralNetwork()
+
+    print("Random initial synaptic weights:")
+    print(neuralnetwork.synaptic_weights)
+
+    input = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
+    output = np.array([0,1,1,0]).T
+    neuralnetwork.train(input,output,1000)
+
+    print("New synaptic weights:")
+    print(neuralnetwork.synaptic_weights)
+
+    # input_one = str(input("Enter first input"))
+    # input_two = str(input("Enter second input"))
+    # input_third = str(input("Enter third input"))
+
+    # inputs = np.array([input_one,input_two,input_third])
+    # neuralnetwork.predict(inputs)
+
+
+
+
